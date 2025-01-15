a0ba59c5058b3ffc75b61cdce8cc7ed4073df98b
maher salman <mahersal001@gmail.com>
2025-01-16 01:52:24 +0200

    edit harhhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    localganache:{
+      url: process.env.PROVIDER_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
